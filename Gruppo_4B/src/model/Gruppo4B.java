e83f584238564c1d9f4cafb826e06cc9a7733190
riccardo-maldera <99264964+riccardo-maldera@users.noreply.github.com>
2022-03-10 12:17:28 +0100

    Update Gruppo4B.java

--- Gruppo_4B/src/model/Gruppo4B.java
+++ Gruppo_4B/src/model/Gruppo4B.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Gruppo4B {
-		public String Riccardo() {}
+		public String Riccardo() {System.out.println("Ciao sono Riccardo Maldera");}
 		public String Sara() {
 		System.out.println("Ciao sono Sara Morrittii");
 		}
