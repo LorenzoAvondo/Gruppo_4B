dcfec23ffc322785da3d0dd3dedfd18b19fc37bb
Lorenzo Avondo <99265034+LorenzoAvondo@users.noreply.github.com>
2022-03-10 12:07:42 +0100

    Add files via upload

---
+++ Gruppo_4B/src/model/Gruppo4B.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Gruppo4B {
+		public String Riccardo() {}
+		public String Sara() {}
+}
---
+++ Gruppo_4B/src/model/Main.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Riccardo r = new Riccardo();
+		Sara s = new Sara();
+	}
+
+}
